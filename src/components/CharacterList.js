038373c452c24ce85a7f2e4859ef794f4e6fa370
martinvilche98 <martinvilche98@hotmail.com>
2021-03-01 07:11:00 -0300

    Implements redux use pattern

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,58 +1,73 @@
-import React, {Component} from 'react';
-import {ScrollView, StyleSheet, View } from 'react-native';
-import {API_URL} from '../api/urls'
+import React, {useState, useEffect} from 'react';
+import {ScrollView, StyleSheet, View, Text} from 'react-native'
 import {CharacterCard} from './CharacterCard'
 import {SearchBar} from './SearchBar'
+import { connect } from 'react-redux'
+import { fetchCharacterList } from '../redux/actions/characterListActions'
 
-export class CharacterList extends Component {
+const CharacterList = (props) => {
 
-	state = {
-		characters: [],
-		filter: "",
+	const {characterListReducer, fetchCharacterList} = props;
+	const {characters} = characterListReducer
+	const [characterList, setCharacterList] = useState([])
+	const [searchFilter, setSearchBarFilter] = useState('')
+
+    useEffect(() => {
+		fetchCharacterList()
+		console.log(characters)
+		setCharacterList(characters)
+    }, [characters])
+
+	// Function called by CharacterCard delete button to remove itself from the list.
+	const onDelete = (name) => {
+		let filteredArray = characterList.filter( (val) => name !== val.name)
+		setCharacterList(filteredArray)
 	}
 
-	componentDidMount(){
-		this.fetchData();
-		this.setState({filterArray: this.state.characters})
+	const setFilter = (name) =>{
+		setSearchBarFilter(name.toLowerCase())
 	}
 
-	fetchData = async () => {
-		const response = await fetch(API_URL);
-		const json = await response.json();
-		this.setState({characters: json.results});
+	const filterMatch = (name) => {
+		return searchFilter.length == 0 || name.toLowerCase().includes(searchFilter)
 	}
 
-	filterItem = (name) => {
-		console.log(name)
-		this.setState({ filter: name });
-	}
-
-	onDelete = (name) => {
-		let arr = this.state.characters;
-		let filteredArray = arr.filter( (val) => name !== val.name)
-		this.setState({ characters: filteredArray });
-	}
-
-	render () {
-		return (
-			<View style={styles.container}>
-				<SearchBar filterMethod={this.filterItem}/>
-      			<ScrollView>
-				{
-					this.state.characters.map((item,index)=>{
-								if(this.state.filter.length == 0 || item.name.includes(this.state.filter))
-									return <CharacterCard key={index} character={item} deleteFunc={this.onDelete}/>
-					})
-				}
-				</ScrollView>
-			</View>
-		)
-	}
+	return (
+		<View style={styles.container}>
+			<SearchBar filterMethod={setFilter}/>
+			<ScrollView>
+			{ characterList == null ? 
+				<View style={styles.loadingContainer}><Text style={styles.loadingText}>Requesting Characters ...</Text></View>: 
+				characterList != null && characterList.map((item,index)=>{
+					if(filterMatch(item.name))
+						return <CharacterCard key={index} character={item} deleteFunc={onDelete}/>
+				})
+			}
+			</ScrollView>
+		</View>
+	)
 }
+export {CharacterListView}
+
+const mapStateToProps = (state) => ({
+	characterListReducer: state.characterListReducer
+})
+
+const CharacterListView = connect( mapStateToProps, {fetchCharacterList} )(CharacterList)
 
 const styles = StyleSheet.create({
 	container: {
 		display: 'flex',
 		justifyContent: 'center'
 	},
+	loadingContainer: {
+		display: 'flex',
+		height: 300,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	loadingText:{
+		color: '#55F',
+		fontWeight: 'bold'
+	}
 });
