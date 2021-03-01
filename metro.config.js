2e584dafb6c9a2e7e0aefda5fe89ee1ca933ce54
martinvilche98 <martinvilche98@hotmail.com>
2021-03-01 07:09:59 -0300

    Adds extensions for compatibility

--- metro.config.js
+++ metro.config.js
@@ -8,7 +8,7 @@
 module.exports = {
   resolver: {
     /* resolver options */
-   sourceExts: ['jsx','js'] //add here 
+    sourceExts: ['jsx','js', 'ts', 'tsx'] // add tsx if its not yet defined
   },
   transformer: {
     /* transformer options */
